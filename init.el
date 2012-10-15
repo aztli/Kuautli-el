cba83f624c31c121dfa39bc4af727b238fce91b4
Miguel Angel Gordian <moon@localhost.localdomain>
2012-10-15 10:50:21 -0500

    zlc - zsh like completion
    zlc provee de un completado al estilo zsh, esto es navegar a travez
    de un menu y la seleccion actual se muestra con un color diferente
    de fondo.
    
    zlc define las teclas de movimiento para navegar a travez del menu.

--- init.el
+++ init.el
@@ -66,4 +66,17 @@ paquete kuautli-el")
   ;; regla de 80 columnas
   (require 'fill-column-indicator)
   (setq-default fill-column 80)
+
+  (require 'zlc)
+
+  (let ((map minibuffer-local-map))
+    ;;; like menu select
+    (define-key map (kbd "<down>")  'zlc-select-next-vertical)
+    (define-key map (kbd "<up>")    'zlc-select-previous-vertical)
+    (define-key map (kbd "<right>") 'zlc-select-next)
+    (define-key map (kbd "<left>")  'zlc-select-previous)
+
+    ;;; reset selection
+    (define-key map (kbd "C-c") 'zlc-reset))
+
 )
